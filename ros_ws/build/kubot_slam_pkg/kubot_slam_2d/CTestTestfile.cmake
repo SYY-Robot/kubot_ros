2764cd711f1e717035d91ba98e460d0b351770ca
kubot080301 <kubot080301>
2021-05-11 08:58:19 +0800

    update

--- ros_ws/build/kubot_slam_pkg/kubot_slam_2d/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rd400/kubot_ros/ros_ws/src/kubot_slam_pkg/kubot_slam_2d
-# Build directory: /home/rd400/kubot_ros/ros_ws/build/kubot_slam_pkg/kubot_slam_2d
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros_ws/build/kubot_slam_pkg/third_party_pkg/slam_gmapping/slam_gmapping/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rd400/kubot_ros/ros_ws/src/kubot_slam_pkg/third_party_pkg/slam_gmapping/slam_gmapping
-# Build directory: /home/rd400/kubot_ros/ros_ws/build/kubot_slam_pkg/third_party_pkg/slam_gmapping/slam_gmapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
