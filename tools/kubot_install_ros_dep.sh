2c96464ff26c68a60e6dab561666fa75d97c4b54
kubot080301 <kubot100601@gmail.com>
2021-06-03 18:12:25 +0800

    update

--- tools/kubot_install_ros_dep.sh
+++ tools/kubot_install_ros_dep.sh
@@ -19,6 +19,10 @@ echo "ros:" $ros_version
 
 if [ "$ros_version" = "melodic" ]; then
 
+    cd ~/kubot_ros/ros_ws/
+    rosdep update
+    rosdep install --from-paths src --ignore-src -r -y --rosdistro melodic
+
     #cd ~/kubot_ros/ros_ws/src/kubot_tools
     #git clone https://github.com/tu-darmstadt-ros-pkg/hector_slam.git
     #git clone https://github.com/orbbec/ros_astra_launch.git
@@ -32,17 +36,17 @@ if [ "$ros_version" = "melodic" ]; then
                 ros-${ros_version}-slam-gmapping \
                 ros-${ros_version}-navigation \
                 ros-${ros_version}-xacro \
-                ros-${ros_version}-yocs-velocity-smoother \
+            #    ros-${ros_version}-yocs-velocity-smoother \
                 ros-${ros_version}-robot-state-publisher \
                 ros-${ros_version}-joint-state-publisher  \
-                ros-${ros_version}-teleop-twist-*  \
+            	ros-${ros_version}-joint-state-publisher-gui \                ros-${ros_version}-teleop-twist-*  \
                 ros-${ros_version}-control-msgs \
                 ros-${ros_version}-kdl-parser-py \
                 ros-${ros_version}-tf2-geometry-msgs \
-                ros-${ros_version}-hector-mapping \
-                ros-${ros_version}-slam-karto  \
-                ros-${ros_version}-hector-geotiff  \
-                ros-${ros_version}-hector-trajectory-server \
+            #    ros-${ros_version}-slam-karto  \
+            #    ros-${ros_version}-hector-mapping \
+            #    ros-${ros_version}-hector-geotiff  \
+            #    ros-${ros_version}-hector-trajectory-server \
                 ros-${ros_version}-usb-cam  \
                 ros-${ros_version}-image-transport \
                 ros-${ros_version}-image-transport-plugins \
@@ -52,25 +56,19 @@ if [ "$ros_version" = "melodic" ]; then
                 ros-${ros_version}-robot-upstart \
                 ros-${ros_version}-tf-conversions \
                 ros-${ros_version}-orocos-kdl \
-                ros-${ros_version}-realsense2-camera \
+            #    ros-${ros_version}-realsense2-camera \
                 ros-${ros_version}-camera-umd \
-		ros-${ros_version}-libuvc* \
+		        ros-${ros_version}-libuvc* \
                 ros-${ros_version}-camera-calibration \
-		ros-${ros_version}-rtabmap* \
-		ros-${ros_version}-cartographer-ros \
-		ros-${ros_version}-joint-state-publisher-gui \
+		    #    ros-${ros_version}-rtabmap* \
+		    #    ros-${ros_version}-cartographer-ros \
+		    #    ros-${ros_version}-cartographer-rviz \
                 ros-${ros_version}-timed-roslaunch \
                 ros-${ros_version}-web-video-server \
-		ros-${ros_version}-robot-pose-ekf 
+		        ros-${ros_version}-robot-pose-ekf 
 else
     exit
 fi
 
-read -s -n1 -p "install ros gui tools?(y/N)" 
 
-if [ "$REPLY" = "y" -o "$REPLY" = "Y" ]; then
-    cd ~/kubot_ros/ros_ws/
-    rosdep update
-    rosdep install --from-paths src --ignore-src -r -y --rosdistro melodic
-fi
 # KUBOT2
