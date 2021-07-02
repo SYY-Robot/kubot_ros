746c47770ed7f880f4f452a878cb1364e6843423
kubot080301 <kubot100601@gmail.com>
2021-07-02 16:30:04 +0800

    fix kubot_install_ros_dep bug

--- tools/kubot_install_ros_dep.sh
+++ tools/kubot_install_ros_dep.sh
@@ -30,23 +30,29 @@ if [ "$ros_version" = "melodic" ]; then
     #git clone https://github.com/ros-drivers/openni2_camera.git
     #git clone https://github.com/ros-drivers/freenect_stack.git
     
+    #sudo apt-get -y --allow-unauthenticated install ros-${ros_version}-ros-base  \
+            #    ros-${ros_version}-yocs-velocity-smoother \
+            #    ros-${ros_version}-slam-karto  \
+            #    ros-${ros_version}-hector-mapping \
+            #    ros-${ros_version}-hector-geotiff  \
+            #    ros-${ros_version}-hector-trajectory-server \
+            #    ros-${ros_version}-realsense2-camera \
+		    #    ros-${ros_version}-rtabmap* \
+		    #    ros-${ros_version}-cartographer-ros \
+		    #    ros-${ros_version}-cartographer-rviz \
+
     cd ..
 
     sudo apt-get -y --allow-unauthenticated install ros-${ros_version}-ros-base  \
                 ros-${ros_version}-slam-gmapping \
                 ros-${ros_version}-navigation \
                 ros-${ros_version}-xacro \
-            #    ros-${ros_version}-yocs-velocity-smoother \
                 ros-${ros_version}-robot-state-publisher \
                 ros-${ros_version}-joint-state-publisher  \
             	ros-${ros_version}-joint-state-publisher-gui \                ros-${ros_version}-teleop-twist-*  \
                 ros-${ros_version}-control-msgs \
                 ros-${ros_version}-kdl-parser-py \
                 ros-${ros_version}-tf2-geometry-msgs \
-            #    ros-${ros_version}-slam-karto  \
-            #    ros-${ros_version}-hector-mapping \
-            #    ros-${ros_version}-hector-geotiff  \
-            #    ros-${ros_version}-hector-trajectory-server \
                 ros-${ros_version}-usb-cam  \
                 ros-${ros_version}-image-transport \
                 ros-${ros_version}-image-transport-plugins \
@@ -56,13 +62,9 @@ if [ "$ros_version" = "melodic" ]; then
                 ros-${ros_version}-robot-upstart \
                 ros-${ros_version}-tf-conversions \
                 ros-${ros_version}-orocos-kdl \
-            #    ros-${ros_version}-realsense2-camera \
                 ros-${ros_version}-camera-umd \
 		        ros-${ros_version}-libuvc* \
                 ros-${ros_version}-camera-calibration \
-		    #    ros-${ros_version}-rtabmap* \
-		    #    ros-${ros_version}-cartographer-ros \
-		    #    ros-${ros_version}-cartographer-rviz \
                 ros-${ros_version}-timed-roslaunch \
                 ros-${ros_version}-web-video-server \
 		        ros-${ros_version}-robot-pose-ekf 
@@ -70,5 +72,4 @@ else
     exit
 fi
 
-
-# KUBOT2
+# KUBOT_ROBOT
