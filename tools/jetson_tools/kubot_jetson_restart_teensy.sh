4a4401839641f2bbef79f60a69706df411dae350
kubot080301 <kubot100601@gmail.com>
2021-11-04 10:58:46 +0800

    debug

--- tools/jetson_tools/kubot_jetson_restart_teensy.sh
+++ tools/jetson_tools/kubot_jetson_restart_teensy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd 
-cd kubot_ros/ros_ws/jetson_tools/
+cd kubot_ros/tools/jetson_tools/
 cc kubot_usb_reset.c -o kubot_usb_reset
 sudo chmod +x kubot_usb_reset
 sudo ./kubot_usb_reset /dev/bus/usb/001/010
