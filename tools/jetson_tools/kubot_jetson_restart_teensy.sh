ba5f5bc4b70273abb3f5f81590036cc30702f029
kubot080301 <kubot100601@gmail.com>
2021-11-04 11:03:33 +0800

    update

--- tools/jetson_tools/kubot_jetson_restart_teensy.sh
+++ tools/jetson_tools/kubot_jetson_restart_teensy.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
+# Yhis will creat c funtion to resey usb device.
 
 cd 
 cd kubot_ros/tools/jetson_tools/
+
+tput setaf 2
+echo "Cmake usb reset ..."
+tput sgr0
+
 cc kubot_usb_reset.c -o kubot_usb_reset
+
+tput setaf 2
+echo "Do usb reset ..."
+tput sgr0
+
+
 sudo chmod +x kubot_usb_reset
 sudo ./kubot_usb_reset /dev/bus/usb/001/010
+
+tput setaf 2
+echo "Finish resey!"
+tput sgr0
